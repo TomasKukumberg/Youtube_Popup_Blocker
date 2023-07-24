7824037cc5797394bc55fb4c294c4d35a0cc6e0a
Tomáš Kukumberg <tomas.kukumberg@centrum.sk>
2023-07-24 17:34:47 +0200

    use map function instead

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-if(document.querySelectorAll('.ytp-ce-element') !== null) {
-    const elements = [...document.querySelectorAll('.ytp-ce-element')];
-    elements.map(element => {
-        element.setAttribute('hidden', true);
-    });
-}
+[...document.querySelectorAll('.ytp-ce-element')].map(el => el.setAttribute('hidden', true));
